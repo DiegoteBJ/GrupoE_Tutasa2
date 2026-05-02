85ada382ef81b52fd5781dd4c4995bb897bef888
santallaluis91 <santallaluis91@gmail.com>
2026-05-02 02:27:36 -0300

    Creando ventana

---
+++ Recepcionar_Despachar_envios_UM/Recepcionar envios UM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrupoE_Tutasa.Recepcionar_Despachar_envios_UM
+{
+    internal class Recepcionar_envios_UM
+    {
+
+    }
+}
---
+++ Recepcionar_Despachar_envios_UM/envios UM Form.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrupoE_Tutasa.Recepcionar_Despachar_envios_UM
+{
+    public partial class envios_UM : Form
+    {
+        public envios_UM()
+        {
+            InitializeComponent();
+        }
+    }
+}
