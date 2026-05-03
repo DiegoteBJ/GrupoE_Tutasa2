8b6d38e440325c432894135f31d06a2852e79c58
ESMERALDA CHURA <Esmeraldachura8@gmai.com>
2026-05-03 19:24:02 -0300

    Recepcion y DEspacho CD Larga Distancia

---
+++ Recepcion y Despacho CD Larga distancia/RecepcionYDespachoLargaDistanciaForm1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace GrupoE_Tutasa.Recepcion_y_Despacho_CD_Larga_distancia
+{
+    partial class RecepcionYDespachoLargaDistanciaForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            button2 = new Button();
+            button3 = new Button();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            listView2 = new ListView();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(36, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(728, 109);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese en Nro de Servicio";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nro. de Servicio";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(211, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(325, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(574, 46);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 30);
+            button1.TabIndex = 2;
+            button1.Text = "BUSCAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(37, 154);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(731, 129);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Datos de la Empresa de Transporte";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(28, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre Transporte:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(28, 86);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 20);
+            label3.TabIndex = 1;
+            label3.Text = "HDR:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ControlLightLight;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonShadow;
+            label4.Location = new Point(193, 40);
+            label4.Name = "label4";
+            label4.Size = new Size(134, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Resultado Nombre";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ControlLightLight;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlDark;
+            label5.Location = new Point(193, 86);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Resultado HDR";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(listView1);
+            groupBox3.Location = new Point(42, 353);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(322, 211);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Guias Recibidas";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(listView2);
+            groupBox4.Location = new Point(429, 353);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(335, 211);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Guias a Despachar";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(130, 595);
+            button2.Name = "button2";
+            button2.Size = new Size(191, 55);
+            button2.TabIndex = 4;
+            button2.Text = "Confirmar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(504, 592);
+            button3.Name = "button3";
+            button3.Size = new Size(207, 48);
+            button3.TabIndex = 5;
+            button3.Text = "Cancelar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            listView1.Location = new Point(12, 30);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(301, 169);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Nro de Guia";
+            columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Tamaño";
+            columnHeader2.Width = 100;
+            // 
+            // listView2
+            // 
+            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader3, columnHeader4, columnHeader5 });
+            listView2.Location = new Point(8, 30);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(310, 168);
+            listView2.TabIndex = 0;
+            listView2.UseCompatibleStateImageBehavior = false;
+            listView2.View = View.Details;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Nro de Guia";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Tamaño";
+            columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Destino";
+            columnHeader5.Width = 100;
+            // 
+            // RecepcionYDespachoLargaDistanciaForm1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 712);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "RecepcionYDespachoLargaDistanciaForm1";
+            Text = "RecepcionYDespachoLargaDistanciaForm1";
+            Load += RecepcionYDespachoLargaDistanciaForm1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private Button button2;
+        private Button button3;
+        private ListView listView2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+    }
+}
---
+++ Recepcion y Despacho CD Larga distancia/RecepcionYDespachoLargaDistanciaForm1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrupoE_Tutasa.Recepcion_y_Despacho_CD_Larga_distancia
+{
+    public partial class RecepcionYDespachoLargaDistanciaForm1 : Form
+    {
+        public RecepcionYDespachoLargaDistanciaForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void RecepcionYDespachoLargaDistanciaForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
