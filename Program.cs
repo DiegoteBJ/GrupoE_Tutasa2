d0cecf087bdc8cbb1dddce8b45ff2fc7e158e7bb
ALEXANDRA <ALEXANDRA@DESKTOP-UH850U9>
2026-05-03 16:24:56 -0300

    CU12 - Actualizar HDR funcionando

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using GrupoE_Tutasa.Entrega_en_Agencia;
 using GrupoE_Tutasa.Entrega_en_CD;
+using GrupoE_Tutasa.Actualizar_HDR;
 
 namespace GrupoE_Tutasa.FormularioPrincipal
 {
@@ -14,9 +15,11 @@ namespace GrupoE_Tutasa.FormularioPrincipal
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             // ApplicationConfiguration.Initialize();
-            Application.Run(new FormularioPrincipal());
+
+            // Application.Run(new FormularioPrincipal());
             // Application.Run(new EntregarEncomiendaAgenciaForm());
             // Application.Run(new EntregaEnCDFormNuevo());
+            Application.Run(new ActualizarHDRForm());
         }
     }
 }
