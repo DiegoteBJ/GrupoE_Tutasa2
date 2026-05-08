18dfdd10b509040ec47556b6c2818476bfa813a4
ESMERALDA CHURA <Esmeraldachura8@gmai.com>
2026-05-07 22:48:45 -0300

    GenerarHDRtransporte

---
+++ GenerarHDRTransporte/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace GrupoE_Tutasa.GenerarHDRTransporte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            listView1 = new ListView();
+            listView2 = new ListView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            columnHeader8 = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(109, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 20);
+            label2.TabIndex = 1;
+            label2.Text = "002";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(209, 22);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 20);
+            label3.TabIndex = 2;
+            label3.Text = "CD: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(251, 22);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 3;
+            label4.Text = "label4";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Location = new Point(17, 57);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(748, 166);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese Nro del Transporte";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(21, 35);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 20);
+            label5.TabIndex = 0;
+            label5.Text = "Nro. de Transporte ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(179, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(297, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(544, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 30);
+            button1.TabIndex = 2;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(28, 82);
+            label6.Name = "label6";
+            label6.Size = new Size(161, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Empresa de Transporte";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(28, 114);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 20);
+            label7.TabIndex = 4;
+            label7.Text = "label7";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(291, 82);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 20);
+            label8.TabIndex = 5;
+            label8.Text = "Fecha";
+            label8.Click += label8_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(286, 120);
+            label9.Name = "label9";
+            label9.Size = new Size(50, 20);
+            label9.TabIndex = 6;
+            label9.Text = "label9";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(454, 86);
+            label10.Name = "label10";
+            label10.Size = new Size(138, 20);
+            label10.TabIndex = 7;
+            label10.Text = "Ruta del Transporte";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(455, 119);
+            label11.Name = "label11";
+            label11.Size = new Size(58, 20);
+            label11.TabIndex = 8;
+            label11.Text = "label11";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(listView1);
+            groupBox2.Location = new Point(26, 276);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(344, 227);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Encomiendas pendientes";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button5);
+            groupBox3.Controls.Add(button4);
+            groupBox3.Controls.Add(listView2);
+            groupBox3.Location = new Point(394, 276);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(378, 227);
+            groupBox3.TabIndex = 6;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Encomiendas asignadas a este HDR";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            listView1.Location = new Point(13, 31);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(320, 135);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // listView2
+            // 
+            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader5, columnHeader6, columnHeader7, columnHeader8 });
+            listView2.Location = new Point(11, 29);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(358, 141);
+            listView2.TabIndex = 0;
+            listView2.UseCompatibleStateImageBehavior = false;
+            listView2.View = View.Details;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(16, 181);
+            button2.Name = "button2";
+            button2.Size = new Size(143, 30);
+            button2.TabIndex = 1;
+            button2.Text = "Agregar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(198, 181);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 37);
+            button3.TabIndex = 2;
+            button3.Text = "Agregar todo";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(21, 184);
+            button4.Name = "button4";
+            button4.Size = new Size(140, 31);
+            button4.TabIndex = 1;
+            button4.Text = "Eliminar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(216, 185);
+            button5.Name = "button5";
+            button5.Size = new Size(153, 33);
+            button5.TabIndex = 2;
+            button5.Text = "Elimitar todo";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(448, 543);
+            button6.Name = "button6";
+            button6.Size = new Size(139, 36);
+            button6.TabIndex = 7;
+            button6.Text = "Eliminar";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(626, 544);
+            button7.Name = "button7";
+            button7.Size = new Size(142, 36);
+            button7.TabIndex = 8;
+            button7.Text = "Generar HDR";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Nro. de Guia";
+            columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Tamaño";
+            columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Peso Kg.";
+            columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Destino";
+            columnHeader4.Width = 70;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Nro. de Guia";
+            columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Tamaño";
+            columnHeader6.Width = 70;
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Peso Kg.";
+            columnHeader7.Width = 70;
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.Text = "Destino";
+            columnHeader8.Width = 70;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 614);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label6;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label5;
+        private Label label8;
+        private Label label10;
+        private Label label9;
+        private Label label11;
+        private GroupBox groupBox2;
+        private Button button2;
+        private ListView listView1;
+        private GroupBox groupBox3;
+        private ListView listView2;
+        private Button button3;
+        private Button button5;
+        private Button button4;
+        private Button button6;
+        private Button button7;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+    }
+}
