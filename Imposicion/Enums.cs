50929229e5247c8154c3527b550e0da98cb5d4a4
Santiago <smbouche@gmail.com>
2026-06-12 22:11:33 -0300

    Reordena enums y añade EN_CD_DESTINO a estados de guía
    Se reordenan los valores de ModalidadImposicionEnum y ModalidadEntregaEnum para mejorar la claridad y se actualizan sus comentarios. Se agrega el estado EN_CD_DESTINO al enum de estados posibles de una guía.

--- Imposicion/Enums.cs
+++ Imposicion/Enums.cs
@@ -12,10 +12,10 @@ namespace GrupoE_Tutasa.Imposicion
     /// <summary>Modalidad en que se impone la encomienda.</summary>
     internal enum ModalidadImposicionEnum
     {
-        /// <summary>C – Imposición directa en CD.</summary>
-        CD,
         /// <summary>A – Imposición en agencia.</summary>
         AGENCIA,
+        /// <summary>C – Imposición directa en CD.</summary>
+        CD,
         /// <summary>D – Pedido de retiro a domicilio (telefónico).</summary>
         DOMICILIO
     }
@@ -23,10 +23,10 @@ namespace GrupoE_Tutasa.Imposicion
     /// <summary>Modalidad de entrega al destinatario.</summary>
     internal enum ModalidadEntregaEnum
     {
-        /// <summary>C – Retiro en CD.</summary>
-        CD,
         /// <summary>A – Retiro en agencia.</summary>
         AGENCIA,
+        /// <summary>C – Retiro en CD.</summary>
+        CD,
         /// <summary>D – Entrega a domicilio.</summary>
         DOMICILIO
     }
@@ -40,6 +40,7 @@ namespace GrupoE_Tutasa.Imposicion
         CANCELADA,
         PENDIENTE_2DO_INTENTO,
         TRASLADADA,
+        EN_CD_DESTINO,
         EN_DISTRIBUCION,
         PENDIENTE_DE_ENTREGA,
         ENTREGADA
