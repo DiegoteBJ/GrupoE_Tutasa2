5436b510233395a162582c03ef3ff53e2e9861d7
Diego Urso <diego_1967@hotmail.com>
2026-06-13 13:07:33 -0300

    Refactor data loading and optimize file copy settings
    Refactored EstadoDeEncomiendaModelo to use MovimientoEstadoGuiaAlmacen for movement data instead of loading from JSON files. Removed all internal JSON deserialization logic. Updated .csproj to set CopyToOutputDirectory to PreserveNewest for data files, improving build efficiency.

--- EstadoDeEncomienda/EstadoDeEncomiendaModelo.cs
+++ EstadoDeEncomienda/EstadoDeEncomiendaModelo.cs
@@ -1,56 +1,27 @@
-﻿using System;
+using GrupoE_Tutasa.Almacenes;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text.Json;
 
 namespace GrupoE_Tutasa.EstadoDeEncomienda
 {
     internal class EstadoDeEncomiendaModelo
     {
-        private List<MovimientoEstadoGuia> movimientos = new List<MovimientoEstadoGuia>();
-
-        public EstadoDeEncomiendaModelo()
-        {
-            CargarMovimientosDesdeJson();
-        }
-
-        private void CargarMovimientosDesdeJson()
-        {
-            string rutaArchivo = Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                "Datos",
-                "MovimientoEstadoGuiaEntidad.json"
-            );
-
-            if (!File.Exists(rutaArchivo))
-            {
-                movimientos = new List<MovimientoEstadoGuia>();
-                return;
-            }
-
-            string json = File.ReadAllText(rutaArchivo);
-
-            movimientos = JsonSerializer.Deserialize<List<MovimientoEstadoGuia>>(
-                json,
-                new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                }
-            ) ?? new List<MovimientoEstadoGuia>();
-        }
-
         public Guia BuscarGuia(int guiaId)
         {
-            List<MovimientoEstadoGuia> movimientosDeLaGuia = movimientos
+            var movimientosDeLaGuia = MovimientoEstadoGuiaAlmacen.movimientoEstadoGuias
                 .Where(m => m.GuiaId == guiaId)
                 .OrderBy(m => m.FechaMovimiento)
+                .Select(m => new MovimientoEstadoGuia
+                {
+                    GuiaId = m.GuiaId,
+                    FechaMovimiento = m.FechaMovimiento,
+                    Estado = m.Estado.ToString(),
+                    Ubicacion = m.Ubicacion
+                })
                 .ToList();
 
             if (!movimientosDeLaGuia.Any())
-            {
                 return null;
-            }
 
             return new Guia
             {
