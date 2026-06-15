b78bd76a27b1303e94abb9d4f24fd90eeb524e63
Diego Urso <diego_1967@hotmail.com>
2026-06-15 15:38:42 -0300

    Ajuste

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GrupoE_Tutasa.FormularioPrincipal
     internal static class Program
     {
         public static int agenciaTrabajoId = 26; // ID de la agencia de trabajo, se puede cambiar según sea necesario
-        public static int CDTrabajoId = 7; // ID del centro de distribución de trabajo, se puede cambiar según sea necesario
+        public static int CDTrabajoId = 5; // ID del centro de distribución de trabajo, se puede cambiar según sea necesario
         public static string modalidadImposicion = "agencia"; // Modalidad de imposición. Valores válidos: "AGENCIA", "CD", "DOMICILIO".
          
         /// <summary>
